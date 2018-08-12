b4936e3f135b6485f0f8ab03a1cebece65773ef3
Andrii <andrii@fream.pl>
2018-08-12 21:55:25 +0100

    rename counterId to id and make it optional

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -2,25 +2,47 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import exactProp from '../utils/exactProp';
 
+/**
+ * @example
+   <Counter
+     value={this.state.value}
+     onChange={this.handlePickerChange}
+     id={entry.id} // optional, will be passed to onChange callback
+     step={2} // optional
+     minValue={10} // optional
+     maxValue={20} // optional
+   >
+     {({ value, increase, decrease }) => (
+        // implement view (web / native)
+        <div>
+          <p>{entry.name}</p>
+          <button onClick={decrease}>-</button>
+          <span>{value}</span>
+          <button onClick={increase}>+</button>
+        </div>
+      )}
+   </Counter>
+ */
+
 class Counter extends Component {
   decrease = () => {
     const {
-      onChange, value, minValue, step, counterId,
+      onChange, value, minValue, step, id,
     } = this.props;
 
     onChange({
-      counterId,
+      id,
       value: value - step >= minValue ? value - step : value,
     });
   };
 
   increase = () => {
     const {
-      onChange, value, maxValue, step, counterId,
+      onChange, value, maxValue, step, id,
     } = this.props;
 
     onChange({
-      counterId,
+      id,
       value: value + step <= maxValue ? value + step : value,
     });
   };
@@ -37,7 +59,7 @@ class Counter extends Component {
 
 Counter.propTypes = {
   children: PropTypes.func.isRequired,
-  counterId: PropTypes.number.isRequired,
+  id: PropTypes.number,
   maxValue: PropTypes.number,
   minValue: PropTypes.number,
   onChange: PropTypes.func.isRequired,
@@ -48,6 +70,7 @@ Counter.propTypes = {
 Counter.propTypes = exactProp(Counter.propTypes);
 
 Counter.defaultProps = {
+  id: null,
   maxValue: 99,
   minValue: 0,
   step: 1,
