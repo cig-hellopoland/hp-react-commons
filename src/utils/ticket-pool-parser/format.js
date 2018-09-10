f0d7ceabf87762fa501da2d10eab8f7251435afc
Janusz Przybylski <janusz@fream.pl>
2018-09-10 11:32:03 +0200

    Passing format options

--- src/utils/ticket-pool-parser/format.js
+++ src/utils/ticket-pool-parser/format.js
@@ -1,6 +1,6 @@
 import formatFn from 'date-fns/format';
 import constants from './constants';
 
-export default function format(date, formatStr = constants.DATE_FORMAT) {
-  return formatFn(date, formatStr);
+export default function format(date, formatStr = constants.DATE_FORMAT, options) {
+  return formatFn(date, formatStr, options);
 }
