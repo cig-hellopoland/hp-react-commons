d8ac77b0a5ffe80575343dadc5376d0d1d207a57
Janusz Przybylski <janusz@fream.pl>
2019-01-28 08:15:09 +0100

    HPL-614 Added ticketPoolDefinitionId to cart entry

--- src/TicketModalController/TicketModalController.js
+++ src/TicketModalController/TicketModalController.js
@@ -299,8 +299,10 @@ class TicketModalController extends Component {
 
   generateCartItem = () => {
     const { date, entries, poolId } = this.state;
-    const { cartItem, sightEvent: product } = this.props;
+    const { availableTickets, cartItem, sightEvent: product } = this.props;
+    const ticketPool = this.getTicketPoolById(availableTickets.ticketPools, poolId) || {};
     const ticketDefinitions = this.getTicketDefinitions();
+    const { ticketPoolDefinitionId } = ticketPool;
 
     const detailedEntries = Object.keys(entries).reduce((acc, entryId) => {
       const entry = _find(ticketDefinitions, { id: +entryId });
@@ -312,6 +314,7 @@ class TicketModalController extends Component {
           date: format(date, constants.DATE_FORMAT),
           entryId: entries[entry.id].entryId,
           poolId,
+          ticketPoolDefinitionId,
           quantity: entries[entry.id].quantity,
         },
       ];
