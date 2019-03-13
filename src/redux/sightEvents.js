e6b390dfda45fe1c1acc4257668dac808c18a53d
wlyskawa <wlyskawa@fream.pl>
2019-03-13 13:42:27 +0100

    redux actions for changing defaultLanguage

--- src/redux/sightEvents.js
+++ src/redux/sightEvents.js
@@ -35,9 +35,34 @@ const prefix = `commons/${name}/`;
  */
 
 /**
+ * Type used for handling change default language request.
+ * @type {string}
+ */
+const CHANGE_DEFAULT_LANGUAGE = `${prefix}CHANGE_DEFAULT_LANGUAGE`;
+
+/**
+ * Type used for handling change default language request cancellation.
+ * @type {string}
+ */
+const CHANGE_DEFAULT_LANGUAGE_CANCEL = `${prefix}CHANGE_DEFAULT_LANGUAGE_CANCEL`;
+
+/**
+ * Type used for handling change default language request failure.
+ * @type {string}
+ */
+const CHANGE_DEFAULT_LANGUAGE_FAILURE = `${prefix}CHANGE_DEFAULT_LANGUAGE_FAILURE`;
+
+/**
+ * Type used for handling change default language request success.
+ * @type {string}
+ */
+const CHANGE_DEFAULT_LANGUAGE_SUCCESS = `${prefix}CHANGE_DEFAULT_LANGUAGE_SUCCESS`;
+
+/**
  * Type used for clearing ticket information for currently loaded entity.
  * @type {string}
  */
+
 const CLEAR_AVAILABLE_TICKETS = `${prefix}CLEAR_AVAILABLE_TICKETS`;
 
 /**
@@ -275,6 +300,10 @@ const STOP_SELL_FAILURE = `${prefix}STOP_SELL_FAILURE`;
 const STOP_SELL_SUCCESS = `${prefix}STOP_SELL_SUCCESS`;
 
 export const types = {
+  CHANGE_DEFAULT_LANGUAGE,
+  CHANGE_DEFAULT_LANGUAGE_CANCEL,
+  CHANGE_DEFAULT_LANGUAGE_FAILURE,
+  CHANGE_DEFAULT_LANGUAGE_SUCCESS,
   CLEAR_AVAILABLE_TICKETS,
   CLEAR_ITEM,
   CLEAR_SEARCH_RESULTS,
@@ -321,6 +350,72 @@ export const types = {
 /*
  * ACTIONS
  */
+
+/**
+ * Creates action for change default language request.
+ * @method
+ * @callback failureCallback
+ * @callback successCallback
+ * @param {Object} params
+ * @param {Object} [params.options] - request config
+ * @param {failureCallback} [params.onFailure] - failure callback
+ * @param {successCallback} [params.onSuccess] - success callback
+ * @return {{
+ *   type: string,
+ *   payload: {url: string, method: string, data: *, options: *},
+ *   onFailure: failureCallback,
+ *   onSuccess: successCallback
+ * }}
+ */
+const changeDefaultLanguage = ({
+  id, options, onFailure, onSuccess,
+} = {}) => ({
+  type: CHANGE_DEFAULT_LANGUAGE,
+  payload: {
+    url: `${apiURL}/${id}/defaultLanguage`,
+    method: 'patch',
+    ...options,
+  },
+  onFailure,
+  onSuccess,
+});
+
+/**
+ * Creates action for change default language request cancelling.
+ * @method
+ * @return {{type: string}}
+ */
+const changeDefaultLanguageCancel = () => ({
+  type: CHANGE_DEFAULT_LANGUAGE_CANCEL,
+});
+
+/**
+ * Creates action for change default language request failing.
+ * @method
+ * @param {Object} params - axios response schema
+ * @param params.data - response body
+ * @param params.status - response status
+ * @return {{
+ *   type: string,
+ *   error: {data, status: number}
+ * }}
+ */
+const changeDefaultLanguageFailure = ({ data, status } = {}) => ({
+  type: CHANGE_DEFAULT_LANGUAGE_FAILURE,
+  error: {
+    data,
+    status,
+  },
+});
+
+/**
+ * Creates action for successful change default language request.
+ * @method
+ * @return {{type: string}}
+ */
+const changeDefaultLanguageSuccess = () => ({
+  type: CHANGE_DEFAULT_LANGUAGE_SUCCESS,
+});
 
 /**
  * Creates action for item removal.
@@ -1038,6 +1133,10 @@ const stopSellSuccess = data => ({
 });
 
 export const actions = {
+  changeDefaultLanguage,
+  changeDefaultLanguageCancel,
+  changeDefaultLanguageFailure,
+  changeDefaultLanguageSuccess,
   clearAvailableTickets,
   clearItem,
   clearSearchResults,
@@ -1151,6 +1250,47 @@ export const selectors = {
  * LOGIC
  */
 
+/**
+ * Logic used for handling change default language request.
+ * @method
+ */
+const changeDefaultLanguageLogic = createLogic({
+  type: [
+    CHANGE_DEFAULT_LANGUAGE,
+  ],
+  async process(
+    { action: { payload, onFailure, onSuccess }, httpClient, cancelled$ },
+    dispatch,
+    done,
+  ) {
+    try {
+      const response = await httpClient.cancellable(payload, cancelled$);
+      const { status } = response;
+
+      if (status === 200 || status === 204) {
+        dispatch(changeDefaultLanguageSuccess());
+
+        if (onSuccess) {
+          onSuccess();
+        }
+      } else {
+        dispatch(changeDefaultLanguageFailure(response));
+
+        if (onFailure) {
+          onFailure();
+        }
+      }
+    } catch ({ response }) {
+      dispatch(changeDefaultLanguageFailure(response));
+
+      if (onFailure) {
+        onFailure();
+      }
+    }
+
+    done();
+  },
+});
 /**
  * Logic used for handling entity search results clearing.
  * @method
@@ -1654,6 +1794,7 @@ const stopSellLogic = createLogic({
 });
 
 export const logic = {
+  changeDefaultLanguageLogic,
   clearSearchResultsLogic,
   createItemLogic,
   createMainImageLogic,
@@ -1714,6 +1855,7 @@ const reducer = (initialState = defaultInitialState) => (state = initialState, a
         error: initialState.error,
         item: initialState.item,
       };
+    case CHANGE_DEFAULT_LANGUAGE_FAILURE:
     case CREATE_ITEM_FAILURE:
     case CREATE_MAIN_IMAGE_FAILURE:
     case CREATE_PDF_FAILURE:
