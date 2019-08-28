103610c4e07df5d8a5ff63b64a0c7b74b34b3fb4
Janusz Przybylski <janusz@fream.pl>
2019-08-28 14:21:20 +0200

    HPL-810 Fixed incorrect date parsing

--- src/utils/ticket-pool-parser/extendDateWithEventTime.js
+++ src/utils/ticket-pool-parser/extendDateWithEventTime.js
@@ -1,14 +1,25 @@
 import getHours from 'date-fns/get_hours';
 import getMinutes from 'date-fns/get_minutes';
+import parse from 'date-fns/parse';
 import setHours from 'date-fns/set_hours';
 import setMinutes from 'date-fns/set_minutes';
-import subMinutes from 'date-fns/sub_minutes';
+import format from './format';
+import validateFnArguments from './validateFnArguments';
 
 export default function extendDateWithEventTime(dateString, eventStartDate) {
-  const d = new Date(dateString);
-  const tzOffset = d.getTimezoneOffset();
+  validateFnArguments(arguments.length, 2);
+
+  if (typeof dateString !== 'string') {
+    throw new TypeError('dateString is expected to be a string.');
+  }
+
+  if (typeof eventStartDate !== 'string') {
+    throw new TypeError('eventStartDate is expected to be a string.');
+  }
+
+  const d = parse(dateString);
   const hours = getHours(eventStartDate);
   const minutes = getMinutes(eventStartDate);
 
-  return setHours(setMinutes(subMinutes(d, tzOffset), minutes), hours);
+  return format(setHours(setMinutes(d, minutes), hours));
 }
--- src/utils/ticket-pool-parser/validateFnArguments.test.js
+++ src/utils/ticket-pool-parser/validateFnArguments.test.js
@@ -4,10 +4,12 @@ describe('Ticket Pool Parser', () => {
   describe('validateFnArguments', () => {
     it('should throw if number of arguments is lower than expeted arguments amount', () => {
       expect(() => validateFnArguments(0, 1)).toThrow();
+      expect(() => validateFnArguments(1, 2)).toThrow();
     });
 
     it('should do nothing if number of passed arguments is equal or greater than expected amount', () => {
       expect(() => validateFnArguments(1, 1)).not.toThrow();
+      expect(() => validateFnArguments(4, 3)).not.toThrow();
     });
   });
 });
